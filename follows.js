08422101057f44a31a371b221551d3b68a684e16
johneato <jge@eato.me.uk>
2013-11-19 10:26:32 +0000

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'zZctmHbR3nWrlcxExIOQlqb6NI72eHHzoMD5gfMF87PUdCSo', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1564378624], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Green House Monitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
